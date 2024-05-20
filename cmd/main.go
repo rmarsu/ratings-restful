6a76b774f25dc58d40b067cc7e0e2c6791420751
r <margaritaau@bk.ru>
2024-05-20 20:10:55 +0000

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,30 @@
 package main
 
 import (
-	""
+	"fmt"
 )
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	if err := initConfig(); err != nil {
+		logrus.Fatal("configerr", err.Error())
+	}
+	if err := godotenv.Load("/home/r_rmarsu/new-project/.env"); err!= nil {
+		logrus.Fatal("enverr", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		logrus.Fatalf("Database Problems: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
